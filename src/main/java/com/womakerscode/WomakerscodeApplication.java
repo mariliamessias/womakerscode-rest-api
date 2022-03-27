eb8e6d515069b1f25d52867f5e091329f33dacbe
mariliamessias <marilia.oliveira@outlook.com>
2022-03-26 22:47:03 -0300

    first commit

---
+++ src/main/java/com/womakerscode/WomakerscodeApplication.java
@@ -0,0 +1,13 @@
+package com.womakerscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomakerscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomakerscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/womakerscode/WomakerscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.womakerscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomakerscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
